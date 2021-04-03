3da0d21ec9d80d74b294afc1344afaadc96e3f1e
FicusThePottedPlant <45177119+FicusThePottedPlant@users.noreply.github.com>
2021-04-04 01:05:22 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from werkzeug.utils import redirect
+from wtforms import SubmitField
+from random import choice
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'сыр'
 
 
 def get_rnd():
-    from random import randint
-    a = randint(1, 26570)
-    data = open('worldcities.csv', encoding='utf-8').readlines()[a]
+    from random import choice
+    data = choice(open('worldcities.csv', encoding='utf-8').readlines())
+    print(data)
+    # data = '"Moscow","Moscow","55.7558","37.6178","Russia","RU","RUS","Moskva","primary","17125000","1643318494"'
     return data.replace('"', '').split(',')[2:4]
 
 
-@app.route('/load')
-def file():
-    return render_template('file.html')
-
-
-@app.route('/load2')
-def street_view():
-    x, y = get_rnd()
-    return render_template('3d.html', lat=float(x), lng=float(y))
-
-
-@app.route('/')
+@app.route('/session', methods=['GET', 'POST'])
 def play():
     x, y = get_rnd()
-    print(x, y)
     return render_template('3d.html', lat=float(x), lng=float(y))
 
 
--- static/3d.js
+++ static/3d.js
@@ -1,40 +1,183 @@
-      let map;
-      let panorama;
+let map
+let myMap
+let panorama
+let cores = 0
+let canMove = true
+let coordinates = []
+let longitude, latitude
+let my
+let sv
 
-      function initialize() {
-        const berkeley = { lat: {{lat}}, lng:{{lng}} };
-        const sv = new google.maps.StreetViewService();
-        panorama = new google.maps.StreetViewPanorama(
-          document.getElementById("pano")
+function modal(text) {
+    let m = document.getElementById("alert")
+    document.getElementById("alert-content").textContent = text
+    m.style.animation = "fade 5s cubic-bezier(0.7, 0, 0.84, 0)"
+    m.onanimationend = () => {
+        m.style.animation = ""
+    }
+}
+
+const pano = () => {
+    return document.getElementById("pano")
+}
+const checkCNT = () => {
+    return document.getElementById("check-country")
+}
+
+function initParams(a, b) {
+    latitude = a
+    longitude = b
+}
+
+function setCorrectLocation() {
+    canMove = false
+    latitude = panorama.getPosition().lat()
+    longitude = panorama.getPosition().lng()
+    function degrees_to_radians(degrees)
+        {
+          var pi = Math.PI;
+          return degrees * (pi/180);
+        }
+    let d = 12742 * Math.asin(Math.sqrt(Math.sin(degrees_to_radians((latitude - coordinates[0]) / 2)) ** 2 + Math.cos(degrees_to_radians(coordinates[0])) * Math.cos(degrees_to_radians(latitude)) * Math.sin(degrees_to_radians((longitude - coordinates[1]) / 2)) ** 2))
+    d = Math.round(d)
+    let r = (5000 - d * 1.3 )
+    r = r < 0 ? 0 : Math.round(r)
+    var myPlacemark = new ymaps.Placemark(
+            coordinates, {iconContent: "Вы выбрали"},{preset: 'islands#blackStretchyIcon',}
         );
-        // Set up the map.
-        map = new google.maps.Map(document.getElementById("map"), {
-          center: berkeley,
-          zoom: 16,
-          streetViewControl: false,
-
+    var myGeoObject = new ymaps.GeoObject({
+            geometry: {
+            type: "LineString",
+            coordinates: [
+                 [latitude,longitude],
+                coordinates
+            ],
+        },
+            // Описываем свойства геообъекта.
+            properties:{
+                hintContent: d + ' км',
+            balloonContentHeader: d + ' км',
+            balloonContentBody: 'Получено ' + r + ' очков',
+            }
+        }, {
+            strokeColor: "#000000",
+            strokeWidth: 5,
+            strokeStyle: 'shortdash',
+            opacity: 0.9,
         });
-        // Set the initial Street View camera to the center of the map
-        sv.getPanorama({ location: berkeley, radius: 5000000000000, source: 'outdoor',preference: google.maps.StreetViewPreference.NEAREST,
-        addressControl:false, enableCloseButton:false,showRoadLabels:false,fullscreenControl:false,
-        }, processSVData);
-      }
+    myMap.geoObjects.add(myPlacemark);
+    myMap.geoObjects.add(myGeoObject);
+    myMap.geoObjects.add(new ymaps.Placemark( [latitude,longitude], {iconContent: "Вы здесь"}, {preset: 'islands#blackStretchyIcon'}))
+    modal(`Вы ошиблись на ${Math.round(d)} км.\n
+     Получено ${r} очков`)
+     myGeoObject.balloon.open(myGeoObject.getCenter());
+}
 
-      function processSVData(data, status) {
-        if (status === "OK") {
-          const location = data.location;
-          const marker = new google.maps.Marker({
+function initialize() {
+    my = {lat: latitude, lng: longitude}
+    sv = new google.maps.StreetViewService()
+    panorama = new google.maps.StreetViewPanorama(
+        pano()
+    )
+    map = new google.maps.Map(document.getElementById("mp"), {
+        center: my,
+        zoom: 16,
+        streetViewControl: false,
+    })
+    sv.getPanorama({
+        location: my,
+        radius: 10000,
+        source: google.maps.StreetViewSource.OUTDOOR,
+        preference: google.maps.StreetViewPreference.BEST
+    }, processSVData)
+}
+
+function processSVData(data, status) {
+    if (status === "OK") {
+        const location = data.location
+        new google.maps.Marker({
             position: location.latLng,
             map,
             title: location.description,
-          });
-          panorama.setPano(location.pano);
-          panorama.setPov({
+        })
+        panorama.setPano(location.pano)
+        panorama.setOptions({
+            streetViewControl: false,
+            rotateView: false,
+            addressControl: false,
+            enableCloseButton: false,
+            showRoadLabels: false,
+            fullscreenControl: false,
+            zoomControl: false,
+            motionTrackingControl: false
+        })
+        panorama.setPov({
             heading: 270,
             pitch: 0,
-          });
-          panorama.setVisible(true);
-          };
+        })
+        panorama.setVisible(true)
+    }
+    else{
+        sv.getPanorama({
+        location: my,
+
+        radius: 1000000,
+        source: google.maps.StreetViewSource.OUTDOOR,
+        preference: google.maps.StreetViewPreference.BEST
+    }, processSVData)}
+}
+
+onload = () => {
+    setTimeout(() => {
+        let l = pano().children[0].children[0].children[0].children[0]
+        let b = pano().children[1]
+        b.parentElement.removeChild(b)
+        l.style.display = "none"
+        pano().style.opacity = 1
+        checkCNT().onclick = () => {
+            document.querySelector(".navigation").setAttribute("result", "")
+            pano().parentElement.setAttribute("unreachable", "")
+            setCorrectLocation()
         }
+    }, 1000)
+}
+ymaps.ready(init)
 
+function init() {
+    myMap = new ymaps.Map("map", {
+        center: [14.3, 90.5],
+        zoom: 3,
+    }, {
+        minZoom:2,
+        balloonMaxWidth: 0,
+        searchControlProvider: 'yandex#search',
+        yandexMapDisablePoiInteractivity: true
+    })
+    myMap.controls.remove('searchControl')
+    myMap.controls.remove('typeSelector')
+    myMap.controls.remove('routeButtonControl')
+    myMap.controls.remove('fullscreenControl')
+    myMap.controls.remove('trafficControl')
+    myMap.controls.remove('rulerControl')
+    myMap.controls.remove('geolocationControl')
+    myMap.controls.remove('zoomControl')
+    myMap.controls.remove('routePanelControl')
+    myMap.cursors.push('arrow')
+    myMap.behaviors.enable('scrollZoom')
 
+    myMap.events.add('click', function (e) {
+        if (canMove) {
+            let coords = e.get('coords')
+            myMap.geoObjects.removeAll()
+            let myPlacemark = new ymaps.Placemark(
+                coords
+            )
+            if (checkCNT().hasAttribute("disabled")) {
+                checkCNT().removeAttribute("disabled")
+            }
+
+            coordinates = coords
+            myMap.geoObjects.add(myPlacemark)
+        }
+    })
+}
