d17c6362edb851b7824604f8801ee41f4ef50528
FicusThePottedPlant <45177119+FicusThePottedPlant@users.noreply.github.com>
2021-04-04 14:12:33 +0500

    Delete test.py

--- templates/test.py
+++
@@ -1,9 +0,0 @@
-import random
-
-data = {i: [] for i in range(100)}
-for i in range(100):
-    a = random.randint(0, 1000)
-    data[a // 10].append(a)
-for i, j in data.items():
-    if j:
-        print(i, j)
