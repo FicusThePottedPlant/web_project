86d084a42321ff782d68d21d9256ad4c5913bcbc
FicusThePottedPlant <45177119+FicusThePottedPlant@users.noreply.github.com>
2021-06-12 23:34:46 +0500

    Update main.py

--- main.py
+++ main.py
@@ -159,4 +159,4 @@ r = requests.get(url)
 user_coords = r.json()['lat'], r.json()['lon']
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
